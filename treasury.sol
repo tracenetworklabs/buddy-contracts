2bb24b8a093e5af7fadfa24fb4adb02eb31cd8d6
prabaldeveloper <prabaljee@gmail.com>
2022-02-02 12:45:26 +0530

    "added function withdrawTokens"

--- treasury.sol
+++ treasury.sol
@@ -180,6 +180,99 @@ interface IERC165Upgradeable {
     function supportsInterface(bytes4 interfaceId) external view returns (bool);
 }
 
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Returns the name of token.
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+
 // File: contracts/@openzeppelin/contracts-upgradeable/utils/introspection/ERC165Upgradeable.sol
 
 pragma solidity >=0.4.24 <0.8.0;
@@ -856,6 +949,23 @@ abstract contract CollateralManagement is AdminRole {
         emit FundsWithdrawn(to, amount);
     }
 
+    /**
+     * @notice Allows an admin to withdraw ERC-20 token funds.
+     * @param tokenAddress ERC-20 token contract address
+     * @param to           Address to receive the withdrawn funds
+     * @param amount       Amount to withdrawal or 0 to withdraw all available funds
+     */
+
+    function withdrawTokens(address tokenAddress, address payable to, uint256 amount)
+        public
+        onlyAdmin
+    {
+        if(amount == 0) {
+            amount = IERC20(tokenAddress).balanceOf(address(this));
+        }
+        IERC20(tokenAddress).transfer(to,amount);
+        
+    }
     uint256[1000] private __gap;
 }
 
