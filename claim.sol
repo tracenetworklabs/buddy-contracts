a73b55f2cc3483bcf3c08723ab49e6fedbdde0f4
KEERTHANA RAMALINGAM <keerthu.ram28@gmail.com>
2022-03-24 16:42:55 +0530

    Updated claim

--- claim.sol
+++ claim.sol
@@ -36,6 +36,125 @@ interface AggregatorV3Interface {
         );
 }
 
+// File: @openzeppelin/contracts/token/ERC20/IERC20.sol
+
+// OpenZeppelin Contracts (last updated v4.5.0) (token/ERC20/IERC20.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `from` to `to` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+// File: @openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol
+
+// OpenZeppelin Contracts v4.4.1 (token/ERC20/extensions/IERC20Metadata.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface for the optional metadata functions from the ERC20 standard.
+ *
+ * _Available since v4.1._
+ */
+interface IERC20Metadata is IERC20 {
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @dev Returns the symbol of the token.
+     */
+    function symbol() external view returns (string memory);
+
+    /**
+     * @dev Returns the decimals places of the token.
+     */
+    function decimals() external view returns (uint8);
+}
+
 // File: contracts/SafeMathUpgradeable.sol
 
 pragma solidity ^0.8.0;
@@ -316,7 +435,7 @@ contract Claim {
     mapping(address => uint256) public payer;
     mapping(address => mapping(uint256 => bool)) public tokenStatus;
 
-    uint256 public claimfee = 25E18; // USD terms
+    uint256 public claimfee = 25; // USD terms
     address public admin;
     uint256 public constant DENOMINATION = 1E18;
 
@@ -344,19 +463,26 @@ contract Claim {
     function getLatestPrice() public view returns (uint256) {
         (, int256 _price, , , ) = priceFeed.latestRoundData();
         uint256 price = uint256(_price).mul(1E10); // converting to E18
-        price = claimfee.mul(DENOMINATION).div(price);
+        price = claimfee.mul(1E18).mul(DENOMINATION).div(price);
         return price;
     }
 
     /**
      * @dev Payfees and claim the NFT to user's wallet
      */
-    function payFees(address collectionAddress, uint256 tokenId)
+    function payFees(address collectionAddress, uint256 tokenId, address payment)
         public
         payable
     {
-        uint256 value = claimfee.mul(getLatestPrice());
-        require(msg.value >= value, "Insufficient fee amount");
+        require(supportedPayment[payment], "Fee payment not supported");
+        if(payment == address(0)) {
+            require(msg.value >= getLatestPrice(), "Insufficient fee amount");
+        }
+        else {
+            uint8 decimals = IERC20Metadata(payment).decimals();
+            IERC20(payment).transferFrom(msg.sender, address(this), claimfee.mul(decimals));
+        }
+        
         payer[msg.sender] = tokenId;
         tokenStatus[msg.sender][tokenId] = true;
         address owner = Collection(collectionAddress).ownerOf(tokenId);
@@ -370,6 +496,14 @@ contract Claim {
     function updateFee(uint256 _fee) public onlyAdmin {
         claimfee = _fee;
         emit FeesUpdated(_fee);
+    }
+
+    /**
+     * @dev Update the fee payment 
+     */
+    function updateFeePayment(address payment, bool status) public onlyAdmin {
+        supportedPayment[payment] = status;
+        emit PaymentUpdated(payment, status);
     }
 
     /**
