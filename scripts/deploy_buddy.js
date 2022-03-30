c01af45fb539f2f40c2a7bb79190cd0ad0d22639
KEERTHANA RAMALINGAM <keerthu.ram28@gmail.com>
2022-03-30 13:44:11 +0530

    Updated

--- scripts/deploy_buddy.js
+++ scripts/deploy_buddy.js
@@ -1,31 +1,35 @@
 const { ethers } = require("hardhat")
 
 async function main() {
-    const buddyTreasury = await ethers.getContractFactory("BuddyTreasury");
-    const treasuryProxy = await upgrades.deployProxy(buddyTreasury, ["0x40a124c4849A25B9b19b2e7aFC4f07302fBb67B1"],{ initializer: 'initialize' })
-    console.log("Treasury proxy", treasuryProxy.address);
-    console.log("Is admin", await treasuryProxy.isAdmin("0x40a124c4849A25B9b19b2e7aFC4f07302fBb67B1"));
+    // const buddyTreasury = await ethers.getContractFactory("BuddyTreasury");
+    // const treasuryProxy = await upgrades.deployProxy(buddyTreasury, ["0x40a124c4849A25B9b19b2e7aFC4f07302fBb67B1"],{ initializer: 'initialize' })
+    // console.log("Treasury proxy", treasuryProxy.address);
+    // console.log("Is admin", await treasuryProxy.isAdmin("0x40a124c4849A25B9b19b2e7aFC4f07302fBb67B1"));
 
     //// **************************/////
 
     const Usx = await ethers.getContractFactory("USX");
-    const USX = await Usx.deploy();
-    await USX.deployed();
+    const USX = await Usx.attach("0x107065A122F92636a1358A70A0efe0F1A080a7e5");
+    // const USX = await Usx.deploy();
+    // await USX.deployed();
 
-    console.log("USX Contract:", USX.address);
-    await USX.initialize("USX", "USX", 18, "1000000000000000000000000");
-    console.log("Old admin", await USX.owner());
+    // console.log("USX Contract:", USX.address);
+    // await USX.initialize("USX", "USX", 18, "1000000000000000000000000");
+
+    // console.log("Old admin", await USX.owner());
     
-    
-    await USX.transferOwnership("0x40a124c4849A25B9b19b2e7aFC4f07302fBb67B1");
+    // await USX.transferOwnership("0x40a124c4849A25B9b19b2e7aFC4f07302fBb67B1");
 
     //// **************************/////
 
     const Buddy = await ethers.getContractFactory("Buddy")
-    const buddyProxy = await upgrades.deployProxy(Buddy, [treasuryProxy.address, "AVATAR", "AVT"],{ initializer: 'initialize' })
-    console.log("Buddy Proxy:", buddyProxy.address)
-    await buddyProxy.adminUpdateToken(USX.address, true, "25000000000000000000", "10000000000000000000");
-    await buddyProxy.transferOwnership("0xdC4A5fC7A3C2dd304F7B44a7954fD4E5cB64c076");
+    const buddyProxy = await Buddy.attach("0xb7531662174615f125D5Dc6F68a274294a0acd22");
+    // const buddyProxy = await upgrades.deployProxy(Buddy, ["0x08B1b86228dBD4e60C16ca8892CE3378dFAbF415", "AVATAR", "AVT"],{ initializer: 'initialize' })
+    // console.log("Buddy Proxy:", buddyProxy.address)
+    // await buddyProxy.adminUpdateToken("0x107065A122F92636a1358A70A0efe0F1A080a7e5", true, "25000000000000000000", "10000000000000000000");
+    await buddyProxy.transferOwnership("0x8E9f0b9E549f0c9d1E996996b482eee10c8B980a");
+
+    
     
     await USX.transfer("0x40a124c4849A25B9b19b2e7aFC4f07302fBb67B1", "1000000000000000000000000");
 
--- scripts/upgrade_claim.js
+++ scripts/upgrade_claim.js
@@ -1,6 +1,6 @@
 async function main() {
-    const Claim = await ethers.getContractFactory("Claim")
-    let proxy = await upgrades.upgradeProxy("Fill me", Claim)
+    const Claim = await ethers.getContractFactory("ClaimV1")
+    let proxy = await upgrades.upgradeProxy("0x68d029Af1130E1Dc7fC82869aAa2188d664CE1f2", Claim)
     console.log("Your upgraded proxy is done!", proxy.address)
 }
 
